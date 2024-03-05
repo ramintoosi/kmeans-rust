7628be0e439506621eca55b3d373476fcd59714f
Ramin Toosi <ramin.tsi@gmail.com>
2024-03-05 18:49:33 +0330

    read file path from arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage:\n./main [file_path]");
+        return
+    }
+    let file_path: &String = &args[1];
+    println!("{file_path}")
 }

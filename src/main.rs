97850da3ffe3aeed4df39a26d45fed49a186e2fd
Ramin Toosi <ramin.tsi@gmail.com>
2024-03-06 18:30:39 +0330

    small changes and first working version

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,10 @@
 use std::{
     env,
-    fs::File,
     error::Error,
 };
 use std::ops::IndexMut;
 use csv::ReaderBuilder;
-use ndarray::{Array2, ArrayView1, AssignElem};
+use ndarray::{Array1, Array2, ArrayView1, AssignElem};
 use ndarray_csv::{Array2Reader};
 use rand::seq::index::sample;
 use rand::thread_rng;
@@ -17,22 +16,26 @@ fn main() {
         println!("Usage:\n./main [file_path]");
         return
     }
-
+    let n_cluster = 4;
     let file_path: &String = &args[1];
     let data = load_data(&file_path).expect("Error reading csv");
-    println!("shape of data: {:?}", data.shape());
-    let mut centers = random_centroids(&data, 10);
-    println!("shape of centers {:?}", centers.shape());
-    let mut indices: Vec<usize> = vec![0; data.nrows()];
-    assign_cluster_to_sample(&data, &centers, &mut indices);
+    // println!("shape of data: {:?}", data.shape());
+    let mut centers = random_centroids(&data, n_cluster);
+    // println!("shape of centers {:?}", centers.shape());
+    let mut indices: Array1<usize> = Array1::zeros(data.nrows());
+    for _ in 0..100{
+        assign_cluster_to_sample(&data, &centers, &mut indices);
+        update_centers(&data, &mut centers, &indices, n_cluster);
+    }
+    println!("Centers:\n{:?}",centers)
+
 
 }
 
 fn load_data(file_path: &str) -> Result<Array2<f32>, Box<dyn Error>> {
 
-    let file = File::open(file_path)?;
-    let mut reader = ReaderBuilder::new().has_headers(false).from_reader(file);
-    let array_read: Array2<f32> = reader.deserialize_array2_dynamic()?;
+    let reader = ReaderBuilder::new().has_headers(false).from_path(file_path);
+    let array_read: Array2<f32> = reader?.deserialize_array2_dynamic()?;
     Ok(array_read)
 }
 
@@ -42,7 +45,7 @@ fn random_centroids(data : &Array2<f32>, n_cluster: usize) -> Array2<f32> {
     let mut rng = thread_rng();
     let mut selected_rows =
         Array2::<f32>::zeros((n_cluster, data.ncols()));
-    let indices: Vec<usize> = sample(&mut rng, n_rows, n_cluster).into_iter().collect();
+    let indices: Array1<usize> = sample(&mut rng, n_rows, n_cluster).into_iter().collect();
     for (i, &index) in indices.iter().enumerate() {
         selected_rows.row_mut(i).assign(&data.row(index));
     }
@@ -54,7 +57,7 @@ fn euclidean_distance(a: ArrayView1<f32>, b: ArrayView1<f32>) -> f32 {
     a.iter().zip(b.iter()).map(|(x, y)| (x - y).powi(2)).sum::<f32>().sqrt()
 }
 
-fn assign_cluster_to_sample(data: &Array2<f32>, centers: &Array2<f32>, indices: &mut Vec<usize>) {
+fn assign_cluster_to_sample(data: &Array2<f32>, centers: &Array2<f32>, indices: &mut Array1<usize>) {
     for (index_sample, row) in data.rows().into_iter().enumerate(){
         let mut min_distance = f32::INFINITY;
         let mut min_index: usize = 0;
@@ -67,5 +70,21 @@ fn assign_cluster_to_sample(data: &Array2<f32>, centers: &Array2<f32>, indices: 
             }
         }
         indices.index_mut(index_sample).assign_elem(min_index)
+    }
+}
+
+fn update_centers(data: &Array2<f32>, centers: &mut Array2<f32>, indices: &Array1<usize>, n_clusters: usize) {
+    for index in 0..n_clusters {
+        let matched_indices: Vec<usize> = indices.iter()
+            .enumerate()
+            .filter(|&(_, &value)| value == index as usize)
+            .map(|(i, _)| i)
+            .collect();
+        let mut c: Array1<f32> = Array1::zeros(data.ncols());
+        for m_index in &matched_indices {
+            c += &data.row(*m_index);
+        }
+        c /= matched_indices.len() as f32;
+        centers.row_mut(index as usize).assign(&c);
     }
 }
